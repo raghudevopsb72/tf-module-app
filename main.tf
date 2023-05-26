7cd7f7c2e11b1aec50e89824e0344b5dd2118c2b
Raghu K <raghuk.vit@gmail.com>
2023-05-26 06:34:15 +0530

    Update tf-module-app/

--- main.tf
+++ main.tf
@@ -44,7 +44,7 @@ resource "aws_autoscaling_group" "asg" {
   name                = "${var.name}-${var.env}-asg"
   desired_capacity    = var.desired_capacity
   max_size            = var.max_size
-  min_size            = var.max_size
+  min_size            = var.min_size
   vpc_zone_identifier = var.subnet_ids
 
 
