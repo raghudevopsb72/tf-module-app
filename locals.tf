1db67cd6cc59b72ff4f346e4a28c14b3a7aa0d1d
Raghu K <raghuk.vit@gmail.com>
2023-05-26 06:56:29 +0530

    Update tf-module-app/

--- locals.tf
+++ locals.tf
@@ -1,3 +1,4 @@
 locals {
-  asg_tags = merge(var.tags, { Name = "${var.name}-${var.env}" })
+  asg_tags         = merge(var.tags, { Name = "${var.name}-${var.env}" })
+  dynamic_asg_tags = [for k, v in local.asg_tags : tomap({ "key" = k, "value" = v })]
 }
