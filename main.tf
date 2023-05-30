5c0dfa96c42cfa611cff4ef48b591a0ec9ac6a6a
Raghu K <raghuk.vit@gmail.com>
2023-05-30 07:21:23 +0530

    Update tf-module-app/

--- main.tf
+++ main.tf
@@ -44,6 +44,7 @@ resource "aws_autoscaling_group" "asg" {
   max_size            = var.max_size
   min_size            = var.min_size
   vpc_zone_identifier = var.subnet_ids
+  target_group_arns   = [aws_lb_target_group.main.arn]
 
 
   launch_template {
